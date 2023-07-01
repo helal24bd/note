d6a63704cd8d530350671d2ae384000e36a6601a
Helal Uddin <54944277+helal24bd@users.noreply.github.com>
2023-07-01 19:50:50 +0600

    Add files via upload
    first commit

---
+++ sql_note_app/create.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var Connection $connection */
+$connection = require_once 'pdo.php';
+
+// Validate note object;
+
+$id = $_POST['id'] ?? '';
+if ($id){
+    $connection->updateNote($id, $_POST);
+} else {
+    $connection->addNote($_POST);
+}
+
+header('Location: index.php');
+
+?>
